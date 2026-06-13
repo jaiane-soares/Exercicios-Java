43372dff7950158a9ffc5676605a70cde419e27c
Jaiane <jaianesoares700@gmail.com>
2026-06-13 19:54:48 -0300

    feat: feito classe telefone

---
+++ src/exerciciopoo/Telefone.java
@@ -0,0 +1,37 @@
+package exerciciopoo;
+
+public class Telefone {
+    private String marca;
+    private String modelo;
+    private String numero;
+    private String ddd;
+    private String tipo;
+
+    public Telefone(String marca, String modelo, String numero, String ddd, String tipo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numero = numero;
+        this.ddd = ddd;
+        this.tipo = tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
